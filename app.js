e0c64835355d5d8a4cbdc87785c2b00612dff0cc
Leider Acevedo <leiderleiderleider@outlook.com>
2019-06-23 21:42:44 -0500

    Este es mi primer commit

---
+++ app.js
@@ -0,0 +1,13 @@
+function registrar(){
+    var correo = document.getElementById('correo').value;
+    var contrasena = document.getElementById('contrasena').value;
+
+    firebase.auth().createUserWithEmailAndPassword(correo, contrasena).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // ...
+      });email-password.html
+      
+      
+}
